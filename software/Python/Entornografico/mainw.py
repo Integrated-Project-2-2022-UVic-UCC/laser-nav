400a995d93b1f792346432f3e0642dfc3923e0d4
Edgar <Gallipoli>
2022-04-08 16:45:21 +0200

    rscale del entorno grafico y fondo

--- software/Python/Entornografico/mainw.py
+++ software/Python/Entornografico/mainw.py
@@ -6,17 +6,25 @@
 #
 # WARNING! All changes made in this file will be lost!
 
-
+##
 from PyQt5 import QtCore, QtGui, QtWidgets
-
-
+##from PyQt5.QtGui import QPixmap,QPainter
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+#
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(800, 601)
-        MainWindow.setStyleSheet("background-color: rgb(12, 129, 255);")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+        MainWindow.setAutoFillBackground(False)
+        self.fondo=QtWidgets.QLabel(self.centralwidget)
+        self.fondo.setGeometry(QtCore.QRect(0, 0, MainWindow.width(),MainWindow.height()))
+        self.fondo.setPixmap(QtGui.QPixmap("fondo.jpg"))
+        self.fondo.setScaledContents(True)
+        #MainWindow.setStyleSheet("background-image: url(fondo.jpg); background-repeat: no-repeat; background-position: center;")
+        print (MainWindow.width(),MainWindow.height())
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setGeometry(QtCore.QRect(50, 50, 231, 61))
         self.label.setObjectName("label")
@@ -34,12 +42,28 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
+        
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.label.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:28pt; font-weight:600; color:#ffaa00;\">Hola Mundo</span></p></body></html>"))
 
 
+    def resizeEvent(self, e):
+        print (e.width(),e.height())
+
+        fondo.resize(e.width(),e.height())
+    def mousePressEvent(self,e):
+        if e.button()==Qt.LeftButton:
+            print("Botó esquerre")
+        elif e.button()==Qt.RightButton:
+            print("Botó dret")
+        elif e.button()==Qt.MidButton:
+            print("Botó mig")
+
+    def mouseMoveEvent(self,e):
+        print(e.x(),e.y())
+        
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
@@ -48,3 +72,4 @@ if __name__ == "__main__":
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
+
---
+++ software/Python/Entornografico/mainx.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'main.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.0
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 601)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(60, 30, 231, 61))
+        self.label.setWordWrap(False)
+        self.label.setIndent(0)
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(190, 70, 111, 131))
+        self.label_2.setText("")
+        self.label_2.setPixmap(QtGui.QPixmap("483625.jpg"))
+        self.label_2.setScaledContents(True)
+        self.label_2.setIndent(-1)
+        self.label_2.setObjectName("label_2")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(150, 60, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+        self.label_2.raise_()
+        self.pushButton.raise_()
+        self.label.raise_()
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:28pt; font-weight:600; color:#ffaa00;\">Hola Mundo</span></p></body></html>"))
+        self.pushButton.setText(_translate("MainWindow", "PushButton"))
+
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
